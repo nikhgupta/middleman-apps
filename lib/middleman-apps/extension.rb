3a5bca1ec88de45bca0838ac9958b6152bcd4278
Nikhil Gupta <me@nikhgupta.com>
2017-10-10 12:19:49 +0530

    refactored for the satisfaction of `reek`

--- lib/middleman-apps/extension.rb
+++ lib/middleman-apps/extension.rb
@@ -1,5 +1,6 @@
 require 'rack/contrib'
 require 'middleman-core'
+require 'middleman-core/load_paths'
 
 module Middleman
   # Middleman extension to load dynamic/modular pages/apps for Middleman.
@@ -7,17 +8,23 @@ module Middleman
     option :not_found, '404.html', 'Path to 404 error page'
 
     class << self
+      ENVIRONMENT = (ENV['MM_ENV'] || ENV['RACK_ENV'] || 'development').to_sym
+
+      OPTIONS = {
+        mode: :config,
+        watcher_disable: true,
+        disable_sitemap: true,
+        exit_before_ready: true,
+        environment: ENVIRONMENT
+      }.freeze
+
       def middleman_app
-        require 'middleman-core/load_paths'
         Middleman.setup_load_paths
 
         ::Middleman::Application.new do
-          config[:mode] = :config
-          config[:exit_before_ready] = true
-          config[:watcher_disable] = true
-          config[:disable_sitemap] = true
-          config[:environment] = (ENV['MM_ENV'] ||
-                                  ENV['RACK_ENV'] || 'development').to_sym
+          OPTIONS.each do |key, val|
+            config[key] = val
+          end
         end
       end
 
@@ -25,11 +32,7 @@ module Middleman
         app = middleman_app
         options = app.extensions[:apps].options.to_h
         ext = new(app, options)
-        app = ext.middleman_static_app
-        ext.modular_apps.each do |path, klass|
-          app.map(path) { run klass }
-        end
-        app
+        ext.mount_modular_apps(ext.middleman_static_app)
       end
     end
 
@@ -42,22 +45,28 @@ module Middleman
     def after_configuration
       create_config_ru
       return unless app.server?
+      mount_modular_apps(app)
+    end
+
+    def mount_modular_apps(rack_app = nil)
+      rack_app ||= app
       modular_apps.each do |path, klass|
-        app.map(path) { run klass }
+        rack_app.map(path) { run klass }
       end
+      rack_app
     end
 
     def create_config_ru
       path = File.join(app.root, 'config.ru')
       return if File.exist?(path)
 
-      content = <<-CONTENT.gsub(/^ {8}/, '')
+      content = <<-CONTENT.gsub(/^ {6}/, '')
       ENV['RACK_ENV'] = 'production'
       require 'middleman-apps'
       run Middleman::Apps.rack_app
       CONTENT
 
-      File.open(path, 'wb') { |f| f.puts content }
+      File.open(path, 'wb') { |file| file.puts content }
     end
 
     def modular_apps
@@ -71,20 +80,23 @@ module Middleman
     def middleman_static_app
       root = File.expand_path(app.config.build_dir.to_s)
       not_found = File.join(root, options.not_found)
-
-      ::Rack::Builder.new do
-        use ::Rack::TryStatic, urls: ['/'],
-                               root: root,
-                               try: ['.html', 'index.html', '/index.html']
-
-        args = File.exist?(not_found) ? [not_found] : []
-        run ::Rack::NotFound.new(*args)
-      end
+      create_static_app root, not_found
     end
 
     def apps_list
       pattern = File.join(app.root, 'apps', '*.rb')
-      Dir[pattern].map { |f| File.realpath(f) if File.file?(f) }.compact
+      Dir[pattern].map do |file|
+        File.realpath(file) if File.file?(file)
+      end.compact
+    end
+
+    def create_static_app(root, not_found)
+      ::Rack::Builder.new do
+        use ::Rack::TryStatic, urls: ['/'], root: root,
+                               try: ['.html', 'index.html', '/index.html']
+        args = File.exist?(not_found) ? [not_found] : []
+        run ::Rack::NotFound.new(*args)
+      end
     end
   end
 end
